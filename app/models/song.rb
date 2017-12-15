94e46787e5c74b91704c7afd40801ca84d4d0198
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-15 05:05:56 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,9 +15,6 @@ class Song < ActiveRecord::Base
     self.artist = Artist.find_or_create_by(name: name)
   end
 
-  def artist_name
-    self.artist.name if self.artist.name
-  end
 
   def note_contents
     self.notes.collect do |note|
